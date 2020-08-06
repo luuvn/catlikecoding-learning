2b5eb72828671a0fc039d1fc67a286b18e221ff7
luu <luu@topsquare.vn>
2020-08-06 16:53:04 +0700

    update

--- movement/own/Assets/Scripts/CustomGravity.cs
+++ movement/own/Assets/Scripts/CustomGravity.cs
@@ -1,25 +1,23 @@
-﻿using System.Numerics;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
-using Vector3 = UnityEngine.Vector3;
+﻿using UnityEngine;
 
 public static class CustomGravity
 {
+
     public static Vector3 GetGravity(Vector3 position)
     {
         return position.normalized * Physics.gravity.y;
-    }
-
-    public static Vector3 GetUpAxis(Vector3 position)
-    {
-        Vector3 up = position.normalized;
-        return up * Physics.gravity.y;
     }
 
     public static Vector3 GetGravity(Vector3 position, out Vector3 upAxis)
     {
         Vector3 up = position.normalized;
         upAxis = Physics.gravity.y < 0f ? up : -up;
-        return upAxis * Physics.gravity.y;
+        return up * Physics.gravity.y;
+    }
+
+    public static Vector3 GetUpAxis(Vector3 position)
+    {
+        Vector3 up = position.normalized;
+        return Physics.gravity.y < 0f ? up : -up;
     }
 }
